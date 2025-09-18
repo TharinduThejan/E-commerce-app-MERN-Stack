72c61e7f2e43bd6bf3b1c09ec9f68eed853066e3
TharinduThejan <141844415+TharinduThejan@users.noreply.github.com>
2025-09-18 19:12:40 +0530

    feat: update Supabase client configuration to use environment variables

--- frontend/src/services/supabaseClient.jsx
+++ frontend/src/services/supabaseClient.jsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wygdrdwatnbwuuuwglbc.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5Z2RyZHdhdG5hbnRid3V1dXdnbGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMjQ4ODksImV4cCI6MjA3MTcwMDg4OX0.UeHYAq2qVXXRfMbuhZdAeLvoTPEXt6UDQLiJYao0FM8";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
